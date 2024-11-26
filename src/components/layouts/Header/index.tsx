eb40431423743d4e6a4c0ac76d19b1bf55ff7661
fitronansori <fitronansori7@gmail.com>
2024-11-26 12:24:16 +0800

    feat: add responsive logo display in header component

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -4,6 +4,8 @@ import MobileMenu from "./MobileMenu";
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import Logo from "@/components/common/Logo";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   const [scroll, setScroll] = useState<boolean>(false);
@@ -26,13 +28,18 @@ const Header = () => {
   return (
     <header
       className={cn(
-        "bg-background sticky top-0 left-0 right-0",
-        scroll && "bg-background z-50 shadow-md transition-all duration-300"
+        "bg-background block",
+        scroll &&
+          "sticky top-0 left-0 right-0 bg-background z-50 shadow-md transition-all duration-300"
       )}>
       <div className="container h-16 flex items-center justify-between">
-        <div>
+        <div className="hidden lg:block">
           <Logo />
         </div>
+
+        <Link href={"/"} className="lg:hidden">
+          <Image src={"/images/logo3.png"} alt="" width={40} height={40} />
+        </Link>
 
         <NavLinks className="hidden lg:flex" />
 
