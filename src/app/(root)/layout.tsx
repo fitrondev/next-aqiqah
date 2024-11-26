fe5a12b8469f4f017b53e8d6512550dbce9568df
fitronansori <fitronansori7@gmail.com>
2024-11-26 12:25:06 +0800

    refactor: remove unused metadata from HomeLayout component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,5 @@
 import Footer from "@/components/layouts/Footer";
 import Header from "@/components/layouts/Header";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Home",
-};
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
