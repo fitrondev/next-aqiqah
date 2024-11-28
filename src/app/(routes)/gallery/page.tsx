e5ab88a0df877185b659170f0ab4b7284165ea17
fitronansori <fitronansori7@gmail.com>
2024-11-28 10:09:24 +0800

    style: center gallery items

--- src/app/(routes)/gallery/page.tsx
+++ src/app/(routes)/gallery/page.tsx
@@ -19,7 +19,7 @@ const GalleryPage = () => {
           className="text-primary"
         />
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 place-items-center  lg:place-items-start">
           {galleryData.map((item, index) => (
             <Image
               key={index}
