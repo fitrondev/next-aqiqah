01256bc0d7c0f83a32054d2df6aae8da655be58e
fitronansori <fitronansori7@gmail.com>
2024-11-28 10:09:37 +0800

    feat: replace default button with WhatsApp button component in Hero section

--- src/components/layouts/Hero/index.tsx
+++ src/components/layouts/Hero/index.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@/components/ui/button";
+import ButtonWA from "@/components/common/ButtonWA";
 import Image from "next/image";
-import Link from "next/link";
-import { FaWhatsapp } from "react-icons/fa6";
 
 const Hero = () => {
   return (
     <section>
-      <div className="container h-[calc(100vh-64px)] flex items-center justify-center">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#f7c3b6,transparent)]"></div>
+      </div>
+
+      <div className="container h-[calc(100vh-64px)] flex items-center justify-center ">
         <div className="size-full lg:h-[500px] grid grid-cols-1 lg:grid-cols-2 place-items-center">
           <div className="lg:flex-1 flex flex-col items-center justify-center lg:items-start lg:justify-start gap-6">
             <h1 className="text-3xl sm:text-5xl lg:text-6xl text-primary font-bold text-center lg:text-start">
@@ -21,11 +23,7 @@ const Hero = () => {
             </p>
 
             <div>
-              <Button asChild>
-                <Link href="/aqiqah">
-                  Pesan Sekarang <FaWhatsapp className="size-5" />
-                </Link>
-              </Button>
+              <ButtonWA />
             </div>
           </div>
 
